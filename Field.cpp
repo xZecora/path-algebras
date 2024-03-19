95067a07e0d26e536fdafae7f7ab5817b005bb28
Frank Moore <moorewf@wfu.edu>
2024-03-19 13:42:02 -0400

    Added naive power function.

--- Field.cpp
+++ Field.cpp
@@ -3,6 +3,15 @@
 // implement logarithmic exponentiation
 FieldElement Field::power(FieldElement a, unsigned int pow)
 {
-   
+   unsigned int newPow = pow % (mCharacteristic - 1);
+   FieldElement result {1};
+   FieldElement curPow {a};
+   while (newPow != 0)
+   {
+      if (newPow % 2) result = multiply(result,curPow);
+      newPow >>= 1;
+      curPow = multiply(curPow,curPow);
+   }
+   return result;
 }
 
